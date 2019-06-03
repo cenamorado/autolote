f2cf097ae31da02b1835c1b7a36f188e18ecb950
Christopher Enamorado <isaaceverywere@gmail.com>
2019-06-03 17:09:35 -0600

    cambios

--- js/ready.js
+++ js/ready.js
@@ -1,32 +1,48 @@
 $(document).ready(function() {
-
-	$('.card-slick').slick({
-		infinite: true,
+  $(".card-slick").slick({
+    infinite: true,
+    cssEase: "linear",
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow:
+      '<div class="slick-next"><i class="fas fa-chevron-right"></i></i></div>',
+    prevArrow:
+      '<div class="slick-prev"><i class="fas fa-chevron-left"></i></i></div>',
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $(".car-slick-detail-main").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".car-slick-detail-img"
+	});
+	$('.car-slick-detail-img').slick({
+		infinite: false,
 		cssEase: 'linear',
 		dots: false,
-		arrows: true,
+		arrows: false,
 		autoplay: false,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		nextArrow:
-			'<div class="slick-next"><i class="fas fa-chevron-right"></i></i></div>',
-		prevArrow:
-			'<div class="slick-prev"><i class="fas fa-chevron-left"></i></i></div>',
-		responsive: [
-			{
-				breakpoint: 990,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-    })
-})
+		slidesToShow: 4,
+		focusOnSelect: true,
+		asNavFor: '.car-slick-detail-main',
+	})
+});
